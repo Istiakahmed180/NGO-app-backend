3a163f4e15e301708a81b86599226dbe62f84017
Istiakahmed180 <istiakahmed180@gmail.com>
2023-10-09 21:28:39 +0600

    admin route get running api

--- routes/adminroute.js
+++ routes/adminroute.js
@@ -5,31 +5,32 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../Scemma/userInfo");
 
+adminRoute.get("/root", (req, res) => {
+  res.send({ message: "Admin Route Is Running " });
+});
+
 // verify Admin Access
 const verifyToken = async (req, res, next) => {
-    try {
-      const { authorization } = req.headers;
-      if (!authorization) {
-        return res.status(403).send({ message: "UnAuthrize Access" });
-      }
-      if (authorization) {
-        const token = authorization.split(" ")[1];
-        const decoded = await jwt.verify(token, `${process.env.JWT_SECRET}`);
-        const { email } = decoded;
-        if (email === req.query.email) {
-          next();
-        } else {
-          return res.status(403).send({ message: "UnAuthrize" });
-        }
-      }
-    } catch (err) {
-      return next("Privet Api");
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(403).send({ message: "UnAuthrize Access" });
     }
-  };
-  
+    if (authorization) {
+      const token = authorization.split(" ")[1];
+      const decoded = await jwt.verify(token, `${process.env.JWT_SECRET}`);
+      const { email } = decoded;
+      if (email === req.query.email) {
+        next();
+      } else {
+        return res.status(403).send({ message: "UnAuthrize" });
+      }
+    }
+  } catch (err) {
+    return next("Privet Api");
+  }
+};
 
-  //  get single author posts 
-
- 
+//  get single author posts
 
 module.exports = adminRoute;
